3b8b648c525afdc7c44f868f2890a4a44cdaaaa5
KADİR <kadircengiz909@gmail.com>
2025-09-01 21:16:18 +0300

    1.09.2025 gün sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,7 +10,7 @@ namespace Business.Constants
     public static class Messages
     {
         public static string ProductAdded = "Ürün eklendi";
-        public static string ProductNameInvalid = "Ürün ismi en az 2 karakter olmalı";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem bakımda";
         public static string ProductListed="Ürünler listelendi";
     }
